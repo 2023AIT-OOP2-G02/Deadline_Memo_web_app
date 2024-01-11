f627c24e5371375835dee69f985da4f51b9cc7d8
k22036 <131113333+k22036@users.noreply.github.com>
2024-01-11 16:10:58 +0900

    first_commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import threading
+
+from flask import Flask, request, render_template, jsonify, Blueprint
+import json  # Python標準のJSONライブラリを読み込んで、データの保存等に使用する
+
+
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
+
+
+# http://127.0.0.1:5000/
+@app.route('/')
+def index():
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    # debugモードが不要の場合は、debug=Trueを消してください
+    app.run(debug=True)
