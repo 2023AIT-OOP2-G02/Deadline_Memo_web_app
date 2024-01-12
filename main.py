7eb1cef9a23011cb157afab31b45c64d888fa52f
K22028 <k22028kk@aitech.ac.jp>
2024-01-12 19:31:03 +0900

    add:flaskで画面遷移の処理

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/add_page", methods=["GET"])
+def add_page():
+    return render_template("add_page.html")
+
+@app.route("/detail_edit_page", methods=["GET"])
+def detail_edit_page():
+    return render_template("detail_edit_page.html")
+
+@app.route("/remove_page", methods=["GET"])
+def remove_page():
+    return render_template("remove_page.html")
+
+@app.route("/search_page", methods=["GET"])
+def search_page():
+    return render_template("search_page.html")
+
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
