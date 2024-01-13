053678805cc99e3d35e2b0ea1ff6bf1b954b18e4
k22036 <131113333+k22036@users.noreply.github.com>
2024-01-13 23:22:56 +0900

    [add] databaseの定義を仕様に準拠するように

--- data_base_init_test.py
+++ data_base_init_test.py
@@ -14,8 +14,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 db = SQLAlchemy(app)
 
 class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))))
+    id = db.Column(db.String(50), primary_key=True) # 課題識別用のID
+    title = db.Column(db.String(120))
+    deadline = db.Column(db.DateTime)
+    subject = db.Column(db.String(120))
+    memo = db.Column(db.String(2000))
+    memo_img = db.Column(db.String(120)) # メモの画像のファイル名をuuidで保存
+    created_by = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))) # 作成日時(日本時間)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))), onupdate=datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))) # 更新日時(日本時間)
     
 with app.app_context():
     db.drop_all()  # データベースの全てのテーブルを削除
