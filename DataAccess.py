3e2718beb818290c25e3d3b8fb61c8a9e29c3fd2
k22036 <131113333+k22036@users.noreply.github.com>
2024-01-14 20:40:13 +0900

    Add fetch_data_all method to DataAccess class

--- DataAccess.py
+++ DataAccess.py
@@ -50,3 +50,19 @@ class DataAccess:
             data.memo_img   = memo_img
             data.created_by = created_by
             db.session.commit()
+            
+    def fetch_data_all(user_id: str) -> str: # json形式で返す
+        data_dict = {}
+        data_list = Data.query.filter_by(created_by=user_id).all()
+        for data in data_list:
+            data_dict[data.id] = {
+                "title"     : data.title,
+                "deadline"  : data.deadline.strftime('%Y-%m-%d %H:%M:%S'),
+                "subject"   : data.subject,
+                "memo"      : data.memo,
+                "memo_img"  : data.memo_img,
+                "created_at": data.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                "updated_at": data.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+                "created_by": data.created_by
+            }
+        return json.dumps(data_dict, ensure_ascii=False)
