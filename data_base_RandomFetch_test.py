271a84d263c604d29fc03ab4cdf35ac1ae99c987
k22036 <131113333+k22036@users.noreply.github.com>
2024-01-22 13:52:24 +0900

    [delete] 不要なtestファイルの削除

--- data_base_RandomFetch_test.py
+++
@@ -1,7 +0,0 @@
-from flask_startup import app
-from DataAccess import DataAccess
-
-if __name__ == "__main__":
-    with app.app_context():
-        res = DataAccess.random_fetch_userID()
-        print(res)
