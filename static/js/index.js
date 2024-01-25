58a67d0e2990b2864328e9d59333cf90a1c62caf
MukPan <mushipan_mc@icloud.com>
2024-01-25 19:06:07 +0900

    update: トップページのスタイルを変更

--- static/js/index.js
+++ static/js/index.js
@@ -4,8 +4,12 @@ import { update_remaining_time } from "./update_remaining_time.js";
 // 日付の表示
 const datetime = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' }, { hour12: false });
 const date = datetime.split(' ')[0];
+//dateの/の直後にスペースを追加
+const dateArr = date.split('/');
+const dateStr = dateArr[0] + ' / ' + dateArr[1] + ' / ' + dateArr[2];
+
 const dateElement = document.getElementById('date');
-dateElement.innerHTML = date;
+dateElement.innerHTML = dateStr;
 
 // ユーザーIDを取得
 const userID = getUserID();
